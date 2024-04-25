75112b4b3400f8ec4d2db4668a6f954e92206f22
inovexaitsolutions <it@innovexaitsolutions.com>
2024-04-25 18:26:17 +0800

    feat: add map

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,15 @@
       <div class="container mx-auto px-4 flex items-center">
         <div class="w-full text-start">
           <div class="flex flex-col align-center">
-            <h2 class="text-4xl md:text-6xl font-bold mb-8 t text-center">About Innovexa IT Solutions</h2>
+            <h2 class="text-4xl md:text-6xl font-bold mb-8 t text-center">
+              About Innovexa IT Solutions
+            </h2>
             <p class="text-lg md:text-2xl mb-8 text-center">
-              Innovexa IT Solutions is a leading solutions provider, dedicated to delivering
-              innovative solutions to clients. <br>We are committed to providing efficient,
-              reliable, and customized technology solutions to businesses, <br>helping them
-              achieve their business goals and enhance competitiveness.
+              Innovexa IT Solutions is a leading solutions provider, dedicated to
+              delivering innovative solutions to clients. <br />We are committed to
+              providing efficient, reliable, and customized technology solutions to
+              businesses, <br />helping them achieve their business goals and enhance
+              competitiveness.
             </p>
             <!-- Add any additional content here -->
           </div>
@@ -26,23 +29,25 @@
       <div class="container mx-auto px-4 flex items-center gap-8 mb-20">
         <div class="w-1/2">
           <!-- Image by freepik -->
-          <img src="/public/index/eye.jpg" alt="Left Image" class="left-img">
+          <img src="/public/index/eye.jpg" alt="Left Image" class="left-img" />
         </div>
         <div class="w-1/2">
-          <h2 class="text-4xl font-bold mb-6 text-[#7b97ca]">Providing Professional Stock Information Consulting Services</h2>
+          <h2 class="text-4xl font-bold mb-6 text-[#7b97ca]">
+            Providing Professional Stock Information Consulting Services
+          </h2>
           <p class="text-lg text-justify leading-6">
             We provide stock advisory services, covering US stock information, industry
             trends, economic data, financial news, etc. We are committed to offering you
-            comprehensive market analysis and investment advice to assist you in making wise
-            investment decisions.
+            comprehensive market analysis and investment advice to assist you in making
+            wise investment decisions.
           </p>
-          <br>
+          <br />
           <p class="text-lg text-justify leading-6">
             Whether you are a novice investor or an experienced trader, we can provide you
             with customized services to meet your needs and goals. With our professional
-            team and extensive information resources, you will be able to better understand
-            the latest trends in the US stock market, seize investment opportunities, and
-            achieve your financial goals.
+            team and extensive information resources, you will be able to better
+            understand the latest trends in the US stock market, seize investment
+            opportunities, and achieve your financial goals.
           </p>
         </div>
       </div>
@@ -61,19 +66,19 @@
             listed on stock exchanges in the United States, such as the New York Stock
             Exchange (NYSE) or the NASDAQ Stock Market.
           </p>
-          <br>
+          <br />
           <p class="text-lg text-justify leading-6 text-[#ffffff]">
-            These stocks represent ownership stakes in companies and can be bought and sold
-            by investors through brokerage firms.
+            These stocks represent ownership stakes in companies and can be bought and
+            sold by investors through brokerage firms.
           </p>
-          <br>
+          <br />
           <p class="text-lg text-justify leading-6 text-[#ffffff]">
-            Investing in US stocks provides individuals with the opportunity to participate
-            in the ownership and potential profits of American companies.
+            Investing in US stocks provides individuals with the opportunity to
+            participate in the ownership and potential profits of American companies.
           </p>
         </div>
         <div class="w-1/2">
-          <img src="/public/index/stocks.jpg" alt="Right Image" class="right-img">
+          <img src="/public/index/stocks.jpg" alt="Right Image" class="right-img" />
         </div>
       </div>
     </section>
@@ -86,19 +91,23 @@
         <div class="w-full flex gap-8 mt-24">
           <div class="w-1/3 card-item yellow">
             <div class="w-full flex flex-col justify-center items-center gap-8">
-              <img src="/public/index/capitalization.svg" alt="icon" class="icon i-yellow">
+              <img
+                src="/public/index/capitalization.svg"
+                alt="icon"
+                class="icon i-yellow"
+              />
               <p class="text-2xl font-bold text-[#333333]">Better Capitalization</p>
             </div>
           </div>
           <div class="w-1/3 card-item blue">
             <div class="w-full flex flex-col justify-center items-center gap-8">
-              <img src="/public/index/liquid.svg" alt="icon" class="icon i-blue">
+              <img src="/public/index/liquid.svg" alt="icon" class="icon i-blue" />
               <p class="text-2xl font-bold text-[#333333]">More Liquidity</p>
             </div>
           </div>
           <div class="w-1/3 card-item green">
             <div class="w-full flex flex-col justify-center items-center gap-8">
-              <img src="/public/index/list.svg" alt="icon" class="icon i-green">
+              <img src="/public/index/list.svg" alt="icon" class="icon i-green" />
               <p class="text-2xl font-bold text-[#333333]">Greater Diversification</p>
             </div>
           </div>
@@ -106,19 +115,19 @@
         <div class="w-full flex gap-8 mt-8">
           <div class="w-1/3 card-item red">
             <div class="w-full flex flex-col justify-center items-center gap-8">
-              <img src="/public/index/market.svg" alt="icon" class="icon i-red">
+              <img src="/public/index/market.svg" alt="icon" class="icon i-red" />
               <p class="text-2xl font-bold text-[#333333]">Market Transparency</p>
             </div>
           </div>
           <div class="w-1/3 card-item purple">
             <div class="w-full flex flex-col justify-center items-center gap-8">
-              <img src="/public/index/low.svg" alt="icon" class="icon i-purple">
+              <img src="/public/index/low.svg" alt="icon" class="icon i-purple" />
               <p class="text-2xl font-bold text-[#333333]">Lower Currency Risk</p>
             </div>
           </div>
           <div class="w-1/3 card-item pink">
             <div class="w-full flex flex-col justify-center items-center gap-8">
-              <img src="/public/index/bulb.svg" alt="icon" class="icon i-pink">
+              <img src="/public/index/bulb.svg" alt="icon" class="icon i-pink" />
               <p class="text-2xl font-bold text-[#333333]">Innovation</p>
             </div>
           </div>
@@ -130,8 +139,23 @@
       <div class="w-full text-center my-20">
         <h2 class="text-6xl font-bold text-[#ffffff]">CONTACT US</h2>
       </div>
-      <div class="container mx-auto px-4 flex items-center gap-8 mb-20">
-       <p class="text-lg">B-15-2, 3, 4, Northpoint Mid Valley, Jalan Medan Syed Putra Utara, 59200 Kuala Lumpur.</p>
+      <div class="container mx-auto px-4 flex items-center gap-8 mb-20 flex flex-col content-center">
+        <div class="w-full text-center text-white">
+          <p class="text-lg">
+            B-15-2, 3, 4, Northpoint Mid Valley, Jalan Medan Syed Putra Utara, 59200 Kuala
+            Lumpur.
+          </p>
+          <p class="text-lg">Email: info@innovexaitsolutions.com</p>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d995.9760589739662!2d101.6770845!3d3.120028!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc498e7b3f64f5%3A0x54f04dce6ddf7aa2!2sNorthpoint!5e0!3m2!1sen!2smy!4v1714040400763!5m2!1sen!2smy"
+          width="1000"
+          height="500"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </section>
   </div>
@@ -165,15 +189,15 @@ img {
   color: #ffffff;
 }
 
-.hero::before{
-  content: '';
+.hero::before {
+  content: "";
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   background-color: black;
-  opacity: .4; 
+  opacity: 0.4;
   z-index: 1;
 }
 
@@ -191,69 +215,69 @@ img {
   z-index: 2;
 }
 
-.left-img{
+.left-img {
   width: 648px;
   height: 486px;
   object-fit: cover;
 }
 
-.right-img{
+.right-img {
   width: 648px;
   height: 486px;
   object-fit: cover;
   float: right;
 }
 
-.icon{
+.icon {
   width: 60px;
   height: 60px;
   object-fit: contain;
 }
 
-.i-blue{
+.i-blue {
   filter: invert(0.5) sepia(1) saturate(5) hue-rotate(175deg);
 }
-.i-yellow{
+.i-yellow {
   filter: invert(0.5) sepia(1) saturate(5) hue-rotate(10deg);
 }
-.i-green{
+.i-green {
   filter: invert(0.5) sepia(1) saturate(5) hue-rotate(100deg);
 }
 
-.i-red{
+.i-red {
   filter: invert(0.5) sepia(1) saturate(5) hue-rotate(320deg);
 }
-.i-purple{
+.i-purple {
   filter: invert(0.5) sepia(1) saturate(5) hue-rotate(220deg);
 }
-.i-pink{
+.i-pink {
   filter: invert(0.5) sepia(1) saturate(5) hue-rotate(275deg);
 }
 
-.card-item{
+.card-item {
   padding: 60px 30px;
   border-radius: 8px;
   border: 1px solid white;
   background-color: #eeeeee;
 }
 
-.card-item.blue:hover{
+.card-item.blue:hover {
   border: 1px solid #7ba0ca;
 }
-.card-item.yellow:hover{
+.card-item.yellow:hover {
   border: 1px solid #cabb7b;
 }
-.card-item.green:hover{
+.card-item.green:hover {
   border: 1px solid #98ca7b;
 }
 
-.card-item.red:hover{
+.card-item.red:hover {
   border: 1px solid #ca7b7b;
 }
-.card-item.purple:hover{
+.card-item.purple:hover {
   border: 1px solid #877bca;
 }
-.card-item.pink:hover{
+.card-item.pink:hover {
   border: 1px solid #ca7ba9;
 }
 </style>
