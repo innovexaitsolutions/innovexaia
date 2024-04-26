b38ebc3decc745d376622c7e33198e0f33788ac4
inovexaitsolutions <it@innovexaitsolutions.com>
2024-04-26 14:45:41 +0800

    fix: navbar components name incorrect

--- app.vue
+++ app.vue
@@ -7,13 +7,13 @@
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
-    Navbar,
-    Footer
-  }
-}
+    NavBar,
+    Footer,
+  },
+};
 </script>
